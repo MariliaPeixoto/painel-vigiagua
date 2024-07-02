c9798150472b923e5741ac6ac57de2d2dfb39641
MariliaPeixoto <160273923+MariliaPeixoto@users.noreply.github.com>
2024-07-02 13:55:02 -0300

    Update app.py

--- app.py
+++ app.py
@@ -3,4 +3,15 @@ import pandas as pd
 import streamlit as st
 import plotly.express as px
 
-st.write('Olá mundo')
+# Configurações da página
+st.set_page_config(
+    page_title="Painel Vigiagua RS",
+    page_icon="	:potable_water:",
+    layout="wide",
+    initial_sidebar_state='collapsed'
+)
+col1, col2, col3 = st.columns([1,4,1])
+
+col3.image('https://github.com/andrejarenkow/csv/blob/master/logo_cevs%20(2).png?raw=true', width=100)
+col2.title('Painel Vigiagua RS')
+col1.image('https://github.com/andrejarenkow/csv/blob/master/logo_estado%20(3)%20(1).png?raw=true', width=150)
