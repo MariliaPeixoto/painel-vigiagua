e1bd02df296cb43db91318d77faddf278ef0a335
MariliaPeixoto <160273923+MariliaPeixoto@users.noreply.github.com>
2024-07-02 15:01:09 -0300

    Update app.py

--- app.py
+++ app.py
@@ -28,7 +28,8 @@ col1, col2, col3, col4 = st.columns(4)
 
 with col1:
     container_filtros = st.container(border=True)
-    crs_selecionada = st.selectbox(label='Selecione a CRS', options=dados_2024['Regional de Saúde'].unique())
+    with container_filtros:
+        crs_selecionada = st.selectbox(label='Selecione a CRS', options=dados_2024['Regional de Saúde'].unique())
 
 # Cálculo da porcentagem para amostras inadequadas GERAL
 with col2:
