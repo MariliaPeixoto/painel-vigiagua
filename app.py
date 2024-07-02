a9ce0d750f077c8b1701864b815ada22e0c6d3d4
MariliaPeixoto <160273923+MariliaPeixoto@users.noreply.github.com>
2024-07-02 13:58:03 -0300

    Update app.py

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ st.set_page_config(
     layout="wide",
     initial_sidebar_state='collapsed'
 )
-col1, col2, col3 = st.columns([1,4,1])
+col1, col2, col3 = st.columns([1,4,1], vertical_alignment="top")
 
 col3.image('https://github.com/andrejarenkow/csv/blob/master/logo_cevs%20(2).png?raw=true', width=150)
 col2.title('VIGIAGUA RS')
