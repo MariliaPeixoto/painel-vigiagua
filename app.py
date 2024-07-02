fd4002ef7c2a0226a2e55b0d0f6c08359ce4989d
MariliaPeixoto <160273923+MariliaPeixoto@users.noreply.github.com>
2024-07-02 14:05:18 -0300

    Update app.py

--- app.py
+++ app.py
@@ -15,3 +15,8 @@ col1, col2, col3 = st.columns([1,4,1], vertical_alignment="center")
 col3.image('https://github.com/andrejarenkow/csv/blob/master/logo_cevs%20(2).png?raw=true', width=150)
 col2.title('VIGIAGUA RS')
 col1.image('https://github.com/andrejarenkow/csv/blob/master/logo_estado%20(3)%20(1).png?raw=true', width=230)
+
+# Amostras dos dados abertos
+
+dados_2024 = pd.read_csv('https://drive.google.com/uc?export=download&id=1aFmCeDug7eJRhTxSwIXxqw_hYHeUwKGC', sep=';')
+dados_2024
