4e21f1295311d05048a9096601c42f9a02410fd5
MariliaPeixoto <160273923+MariliaPeixoto@users.noreply.github.com>
2024-07-04 12:45:45 -0300

    Update app.py

--- app.py
+++ app.py
@@ -76,8 +76,9 @@ with col4:
         st.metric(label = 'Amostras não validadas', value=f'{amostras_nao_validadas_total}')
 
 # Transformar a tabela em dinâmica
-parametros = pd.pivot_table(dados_2024, index='Município', aggfunc='size').reset_index(drop=True)
+parametros = pd.pivot_table(dados_2024, index='Município', aggfunc='size').reset_index()
+parametros
 
 # Juntar tabelas dos dados dos municípios com o resultado das análises
-tabela_mapa = parametros.merge(muni, left_on='Código IBGE', right_on='IBGE6', how='right')
-tabela_mapa
+#tabela_mapa = parametros.merge(muni, left_on='Código IBGE', right_on='IBGE6', how='right')
+#tabela_mapa
